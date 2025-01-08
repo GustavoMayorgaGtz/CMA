9b0301795638f10f6109cc2e3c945d7f14338911
GustavoMayorgaGtz <redvioletoficial@gmail.com>
2025-01-08 11:01:49 -0600

    feat: Add Event Listeners for Push Buttons Revision 1

---
+++ examples/CMA_INDICADOR/CMA_INDICADOR/CMA_INDICADOR.ino
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include <CMA.h>
+
+// Configuración de conexiones con el servidor CMA
+CMA CMA_SOCKET;
+
+// Credenciales de la red WiFi
+char* ssid = "CyberSphere2.4";        // Nombre de la red WiFi
+char* pass = "KD6JVB8kmCRe";    // Contraseña de la red WiFi
+
+/**
+ * @brief Función de configuración inicial de Arduino.
+ */
+void setup() {
+  Serial.begin(9600);
+  
+  // Conectar al servidor de CMA
+  CMA_SOCKET.connectToWiFiAndServer(ssid, pass);
+  CMA_SOCKET.setMessageSendInterval(10000); // Establecer el envío de datos cada 10 segundos
+
+  // Evento de presión del botón
+  CMA_SOCKET.onPushButtonOnEvent("1736352988319", 
+    []() {
+      Serial.println("Push button event on");
+    }
+  );
+
+  // Evento de soltar el botón
+  CMA_SOCKET.onPushButtonOffEvent("1736352988319", 
+    []() {
+      Serial.println("Push button event off");
+    }
+  );
+
+    // Evento de presión del botón
+  CMA_SOCKET.onPushButtonOnEvent("1736353030357", 
+    []() {
+      Serial.println("Push button event on 2");
+    }
+  );
+
+  // Evento de soltar el botón
+  CMA_SOCKET.onPushButtonOffEvent("1736353030357", 
+    []() {
+      Serial.println("Push button event off 2");
+    }
+  );
+}
+
+/**
+ * @brief Bucle principal que se ejecuta continuamente.
+ */
+void loop() {
+  delay(8);
+  CMA_SOCKET.loop();
+  
+  // Enviar mensajes a los grupos
+  CMA_SOCKET.sendMessageToGroup("1733865367813", 2);
+  CMA_SOCKET.sendMessageToGroup("1733865397296", 3);
+  CMA_SOCKET.sendMessageToGroup("1733865419974", 4);
+  CMA_SOCKET.sendMessageToGroup("1733865475840", 5);
+
+  // // Evento que se ejecuta cuando se mantiene presionado el botón
+  // CMA_SOCKET.onPushButtonOn("1736352988319", 
+  //   []() {
+  //     Serial.println("Push button on 1");
+  //   }
+  // );
+
+  // // Evento que se ejecuta cuando se suelta el botón
+  // CMA_SOCKET.onPushButtonOff("1736352988319", 
+  //   []() {
+  //     Serial.println("Push button off 1");
+  //   }
+  // );
+}
+
---
+++ src/CMA.h
@@ -0,0 +1,83 @@
+#ifndef CMA_H
+#define CMA_H
+
+#include <ArduinoJson.h>
+#include <WebSocketsClient.h>
+#include <WebSockets.h>
+#include <SocketIOclient.h>
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+// Definir una macro para debug
+#define CMA_DEBUG 1 // Actívalo (1) o desactívalo (0)
+
+#if CMA_DEBUG
+    #define DEBUG_PRINT(x)   Serial.print(x)
+    #define DEBUG_PRINTF(...) Serial.printf(__VA_ARGS__)
+    #define DEBUG_PRINTLN(x) Serial.println(x)
+#else
+    #define DEBUG_PRINT(x)
+    #define DEBUG_PRINTLN(x)
+    #define DEBUG_PRINTF(...)
+#endif
+
+// Configuración de WiFi
+extern WiFiMulti wifiManager;  // Cambiado de Wifi a wifiManager
+
+extern bool socketConnected;  // Cambiado de isConnectSocket a socketConnected
+extern void socketIOEventHandler(socketIOmessageType_t type, uint8_t *payload, size_t length); // Cambiado el nombre de la función
+extern SocketIOclient socketClient;  // Cambiado de client_io a socketClient
+extern int messageSendInterval;  // Cambiado de waiting_send_time a messageSendInterval
+extern unsigned long currentTime;  // Cambiado de time_now a currentTime
+extern unsigned long previousTime;  // Cambiado de time_aux a previousTime
+
+// Estructura para manejar los tiempos de los grupos
+struct GroupTime
+{
+    String groupName;  // Cambiado de groupname a groupName
+    long timestamp;    // Cambiado de time a timestamp
+    GroupTime* nextNode;  // Cambiado de time_next a nextNode
+};
+
+struct PushButton
+{
+    String groupName;  // Cambiado de groupname a groupName
+    String status;        // Cambiado de status a status
+    bool joined;
+    std::function<void()> onPushButtonOnCallback;
+    std::function<void()> onPushButtonOffCallback;
+    PushButton* nextNode;  // Cambiado de servo_next a nextNode
+};
+
+// Funciones externas
+extern void appendGroupTime(String groupName, long timestamp);  // Cambiado de append a appendGroupTime
+extern long getGroupTimestamp(String groupNameSearch);  // Cambiado de timeByGroupname a getGroupTimestamp
+extern void updateGroupTimestamp(String groupNameSearch, long newTimestamp);  // Cambiado de settimeByGroupname a updateGroupTimestamp
+void joinGroup(String groupName); // Unirse a un grupo de websocket
+
+class CMA
+{
+public:
+    CMA();
+    // ---------------------------------------- UTILS ---------------------------------------- //
+    static StaticJsonDocument<200> parseJsonData(String data);  // Cambiado de getData a parseJsonData
+    void connectToWiFiAndServer(char* ssid, char* pass);  // Cambiado de connect a connectToWiFiAndServer
+    
+    void setMessageSendInterval(int interval);  // Cambiado de set_refresh_time a setMessageSendInterval
+    // ---------------------------------------- INDICADORES ---------------------------------------- //
+    void sendMessageToGroup(String groupName, float messageData = NAN);  // Cambiado de sendToGroup a sendMessageToGroup
+    // ---------------------------------------- PULSADORES ---------------------------------------- //
+    void onPushButtonOn(String groupName, const std::function<void()>& callback); // Callback para cuando se mantiene presiona el botón
+    void onPushButtonOff(String groupName, const std::function<void()>& callback); // Callback para cuando se suelta el botón
+    void onPushButtonOnEvent(String groupName, const std::function<void()>& callback); // Callback para evento de presión
+    void onPushButtonOffEvent(String groupName, const std::function<void()>& callback); // Callback para evento de soltar
+    // ---------------------------------------- LOOP ---------------------------------------- //
+    void loop();
+
+private:
+    void initializeDebugger();
+};
+
+#endif
+
